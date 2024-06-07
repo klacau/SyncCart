1155771e26cfb93353fb05732585853e97a2e121
a1kotori <aokikotori@gmail.com>
2024-06-08 00:36:59 +0300

    Add router

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,32 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import { createWebHistory, createRouter } from 'vue-router';
+
+import HomePage from './components/WelcomePage.vue';
+import WelcomePage from './components/WelcomePage.vue';
+
+const routes = [
+    { 
+        path: '/', 
+        component: HomePage, 
+        beforeEnter: (to, from) => {
+            return { name: 'Welcome' };
+        }
+    },
+    {
+        path: '/welcome',
+        name: 'Welcome',
+        component: WelcomePage
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+createApp(App)
+    .use(router)
+    .mount('#app');
