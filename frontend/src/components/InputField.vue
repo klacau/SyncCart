6cce583d22da3de75dd29ca0c64e845a043139bc
Романов Данил <danielo.sarcapone@gmail.com>
2024-06-10 00:18:46 +0300

    SignUpPage

---
+++ frontend/src/components/InputField.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+interface FieldProps {
+    label: string
+}
+const props = defineProps<FieldProps>()
+</script>
+
+<template>
+    <div class="field">
+        <p class="field-header">
+            {{props.label}}
+        </p>
+        <input class="field-input" type="text"></input>
+    </div>
+</template>
+<style>
+    .field{
+        width: 100%;
+        margin-bottom: 16px;
+    }
+    .field-input{
+        height: 40px;
+        width: 100%;
+        padding: 8px;
+        box-sizing: border-box;
+        border: 1px solid #E2E3E7;
+        border-radius: 12px;
+    }
+    .field-header{
+        line-height: 16px;
+        font-family: Inter;
+        font-size: 13px;
+        color: #6E7C89;
+    }
+</style>
--- frontend/src/components/SignUpPage.vue
+++ frontend/src/components/SignUpPage.vue
@@ -1,33 +1,77 @@
 <script setup lang="ts">
 import logoUrl from '../assets/logotype.svg?url'
+import Button from './Button.vue'
+import InputField from './InputField.vue'
+import {useRouter} from 'vue-router'
+const router = useRouter();
 </script>
 
 <template>
     <div class="sign-up-content-wrapper">
         <div class="sign-up-content">
             <header class="header-nav">
-                <div class="header-nav-logo">
-                    <img :src="logoUrl" alt="logo">
+                <div class="header-nav-logo" @click="() => router.push({name:'Welcome'})">
+                    <img class="header-logo__img" :src="logoUrl" alt="logo">
                 </div>
             </header>
         </div>
     </div>
+    <div class="sign-up-main-content-wrapper">
+        <div class="sign-up-main-content">
+            <h2 class="sign-up-main-content-h2">Регистрация</h2>
+            <InputField label="E-mail"/>
+            <InputField label="Имя"/>
+            <InputField label="Пароль"/>
+            <Button theme="primary" text="Зарегистрироваться" class="button-register"/>
+        </div>
+    </div>    
 </template>
 
 <style>
+    .header-logo__img{
+        cursor: pointer;
+    }
     .sign-up-content-wrapper{
         display: flex;
         justify-content: space-around;
     }
     .sign-up-content{
+        display: flex;
+        flex-direction: column;
         flex-grow: 1;
         max-width: 1200px;
     }
     .header-nav{
         display: flex;
+        position: fixed;
     }
     .header-nav-logo{
         display: flex;
         padding: 24px;
+    }
+    .sign-up-main-content-wrapper{
+        display: flex;
+        justify-content: space-around;
+        margin-top: 20vh;
+    }
+    .sign-up-main-content-h2{
+        justify-content: center;
+        padding: 40px;
+        line-height: 28px;
+        font-family: Inter;
+        font-size: 24px;
+        font-weight: bold;
+    }
+    .sign-up-main-content{
+        flex: 1;
+        max-width: 400px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 24px;
+    }
+    .button-register{
+        width: 100%;
     }
 </style>
